2fa1a207b93c53c806f0be78671f3169a5869ac1
yiqian <1263678911@qq.com>
2024-01-04 23:32:02 +0800

    feat: 全局引入组件

--- App.vue
+++ App.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         start test
+        <LlButton type="primary">test</LlButton>
+        <LlButton>test2</LlButton>
     </div>
 </template>
 <script setup lang="ts">
-
 </script>
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue'
+import lotus from "@lotus-leaf/mini-app";
 
 const app = createApp(App)
+app.use(lotus)
 app.mount('#app')
