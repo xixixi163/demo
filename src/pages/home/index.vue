051bd9bdcf8e0c34597179f998ab89d82515781c
yiqian <1263678911@qq.com>
2024-11-21 16:36:44 +0800

    feat: home route

---
+++ src/pages/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        start test
+        <LlButton @click="handleTo('./list')">virtual list</LlButton>
+        <LlButton @click="handleTo('./model')">v-model</LlButton>
+        <LlButton type="primary">test</LlButton>
+        <LlButton @click="handleDestoryForm">Form销毁</LlButton>
+       <i>hello world</i>
+       <em>hello world</em>
+       <Form v-if="isShow"></Form>
+       <CompAttrs/>
+    </div>
+</template>
+<script lang="ts" setup>
+import Form from "@/components/Form/Form.vue"
+import CompAttrs from "@/components/CompAttrs/CompAttrs.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter()
+const isShow = ref(true)
+const handleDestoryForm = () => {
+    isShow.value = !isShow.value
+}
+const handleTo = (path: string) => {
+    router.push(path)
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
