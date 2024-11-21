50417edd2cc5006c56b6678eb4a3f393d66c661b
yiqian <1263678911@qq.com>
2024-11-21 16:00:25 +0800

    feat: virtual list

---
+++ src/pages/list/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <LlVirtualList></LlVirtualList>
+    </div>
+</template>
+<script setup lang="ts">
+    
+</script>
+<style lang="scss" scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from "@vitejs/plugin-vue"
-
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    }
 })
